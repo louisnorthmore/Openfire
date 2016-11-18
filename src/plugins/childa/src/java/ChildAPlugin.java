43f37f18dc5a6790d700d0b3bbea5878acce9dcc
Guus der Kinderen <guus.der.kinderen@gmail.com>
2016-11-18 11:11:46 +0100

    Dummy plugins that exist in parent/child hierarchy.

---
+++ src/plugins/childa/src/java/ChildAPlugin.java
@@ -0,0 +1,8 @@
+public class ChildAPlugin implements org.jivesoftware.openfire.container.Plugin
+{
+    public void initializePlugin(org.jivesoftware.openfire.container.PluginManager manager, java.io.File pluginDirectory)
+    {}
+
+    public void destroyPlugin()
+    {}
+}
---
+++ src/plugins/childb/src/java/ChildBPlugin.java
@@ -0,0 +1,8 @@
+public class ChildBPlugin implements org.jivesoftware.openfire.container.Plugin
+{
+    public void initializePlugin(org.jivesoftware.openfire.container.PluginManager manager, java.io.File pluginDirectory)
+    {}
+
+    public void destroyPlugin()
+    {}
+}
